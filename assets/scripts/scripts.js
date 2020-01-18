98caa76d26472bbb79cc087f66821f66c23efd7f
Sam Randels <samrandels@gmail.com>
2020-01-18 16:05:28 -0500

    Change button styling and add foundation

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,29 @@
+// EVENT HANDLERS FOR MAIN BUTTON AREA ON CARDS
+$(".card0 button, .githubMain").on("click", function() {
+  window.open("https://github.com/magiama9", "_blank"); // MY GITHUB PROFILE
+});
+
+
+$(".card1 button").on("click", function() {
+  window.open("https://magiama9.github.io/group-project-01/", "_blank"); // COMMUTEBUDDY SITE
+});
+
+$(".card2 button").on("click", function() {
+  window.open("https://magiama9.github.io/password-generator/", "_blank"); // PASSWORD GEN SITE
+});
+
+$(".card3 button").on("click", function() {
+  window.open("https://magiama9.github.io/code-quiz/", "_blank"); // CODE QUIZ SITE
+});
+
+$(".githubBuddy").on("click", function() {
+  window.open("https://github.com/magiama9/group-project-01", "_blank"); // COMMUTEBUDDY REPO
+});
+
+$(".githubnKey").on("click", function() {
+  window.open("https://github.com/magiama9/password-generator", "_blank"); // nKey REPO
+});
+
+$(".githubQuiz").on("click", function() {
+  window.open("https://github.com/magiama9/code-quiz", "_blank"); // CODE QUIZ REPO
+});
